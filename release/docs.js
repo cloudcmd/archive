855181adf1a28be6f5c730fd828c809e744acb61
coderaiser <mnemonic.enemy@gmail.com>
2015-04-09 05:45:44 -0400

    fix(docs) new versions did not added

--- release/docs.js
+++ release/docs.js
@@ -1,38 +1,35 @@
 (function() {
     'use strict';
     
-    var DIR         = '../',
-        
-        cl          = require('./cl'),
+    var cl          = require('./cl'),
         pack        = require('./pack'),
         place       = require('place'),
         rendy       = require('rendy'),
-        shortdate   = require('shortdate'),
-        Info        = require(DIR + 'package');
+        shortdate   = require('shortdate');
         
     module.exports = function(callback) {
-        var history     = 'Version history\n---------------\n',
+        var history     = '---------------\n',
             ext         = '.tar.gz',
             link        = '//github.com/cloudcmd/archive/raw/master/cloudcmd',
             template    = '- *{{ date }}*, '    +
                           '**[v{{ version }}]'   +
-                          '(' + link + '-v{{ version }}{{ extension }})**\n',
-            version     = Info.version;
+                          '(' + link + '-v{{ version }}{{ extension }})**\n';
         
         cl(function(e, versionNew) {
-            if (!error(e, callback))
-                replaceVersion('README.md', version, versionNew, function() {
-                    var historyNew = history + rendy(template, {
-                        date        : shortdate(),
-                        version     : versionNew,
-                        extension   : ext
-                    });
-                    
-                    replaceVersion('README.md', history, historyNew, function(e) {
-                        if (!error(e, callback))
-                            pack(versionNew, callback);
-                    });
+            var historyNew;
+            
+            if (!error(e, callback)) {
+                historyNew = history + rendy(template, {
+                    date        : shortdate(),
+                    version     : versionNew,
+                    extension   : ext
                 });
+                
+                replaceVersion('README.md', history, historyNew, function(e) {
+                    if (!error(e, callback))
+                        pack(versionNew, callback);
+                });
+            }
         });
     };
     
