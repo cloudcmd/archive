c7fbf5f7a5b476beaa80e11e4dc2ef3eb373a13e
coderaiser <coderaiser@cloudcmd.io>
2016-12-05 14:55:01 +0200

    chore(archive) rm iife

--- bin/release.js
+++ bin/release.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
-    
-    var DIR         = '../lib/',
-        cl          = require(DIR + 'cl'),
-        docs        = require(DIR + 'docs'),
-        pack        = require(DIR + 'pack');
-    
-    cl(function(e, versionNew) {
-        if (!error(e)) {
-            docs(versionNew, function(e, msg) {
-                error(e) || console.log(msg);
-            });
-            
-            pack(versionNew, error);
-        }
-    });
-    
-    function error(e) {
-        e && console.error(e.message);
-        return e;
+'use strict';
+
+var DIR         = '../lib/',
+    cl          = require(DIR + 'cl'),
+    docs        = require(DIR + 'docs'),
+    pack        = require(DIR + 'pack');
+
+cl(function(e, versionNew) {
+    if (!error(e)) {
+        docs(versionNew, function(e, msg) {
+            error(e) || console.log(msg);
+        });
+        
+        pack(versionNew, error);
     }
-    
-})();
+});
+
+function error(e) {
+    e && console.error(e.message);
+    return e;
+}
+
