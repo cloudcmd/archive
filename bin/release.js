74739636cb98518dcbd6dc2c3fe2254d6ccc8696
coderaiser <coderaiser@cloudcmd.io>
2017-05-15 18:04:59 +0300

    fix(release) rm cl, pack

--- bin/release.js
+++ bin/release.js
@@ -3,13 +3,11 @@
 'use strict';
 
 const DIR = '../lib/';
-const cl = require(DIR + 'cl');
 const docs = require(DIR + 'docs');
-const pack = require(DIR + 'pack');
 
 const version = process.env.WISDOM_VERSION;
 
-docs(versionNew, (e, msg) => {
+docs(version, (e, msg) => {
     error(e) || console.log(msg);
 });
 
